70a85fee5f2802d216867340cd6b64885f0b8460
tanzs <minjingtzs@163.com>
2020-12-09 15:20:54 +0800

    静态echarts切换模板

---
+++ src/views/chat/index.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="dashboard-container">
+    <div class="dashboard-editor-container">
+      <el-row style="background:#fff;padding:16px 16px 0;margin-bottom:32px;">
+        <div>
+          <el-radio-group v-model="type" size="small" @change="changeType">
+            <el-radio-button label="1">总销售额</el-radio-button>
+            <el-radio-button label="2">分类销售额</el-radio-button>
+          </el-radio-group>
+        </div>
+          <now-chart v-bind:type="type"/>   //传值
+       </el-row>
+    </div>
+  </div>
+</template>
+
+
+<script>
+import NowChart from '@/views/chat/chats'  //引用echarts组件
+
+export default {
+  name: 'Dashboard',
+  data(){
+     return {
+      type:'1',
+     }
+  },
+  components: {
+    NowChart
+  },
+   methods:{
+      changeType(value){
+         console.log("当前选中:"+value)
+         this.type=value;
+      }
+   }
+}
+</script>
+
+<style rel="stylesheet/scss" lang="scss" scoped>
+  .dashboard-editor-container {
+    padding: 18px 22px 22px 22px;
+    background-color: rgb(240, 242, 245);
+    .chart-wrapper {
+      background: #fff;
+      padding: 16px 16px 0;
+      margin-bottom: 32px;
+    }
+  }
+</style>
